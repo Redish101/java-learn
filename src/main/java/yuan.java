5ab01cf99888b0eda96764e64a77a07467ea5245
Redish101 <jiayunluo@outlook.com>
2022-04-08 09:51:43 +0800

    b

--- src/main/java/yuan.java
+++ src/main/java/yuan.java
@@ -6,9 +6,8 @@ public class yuan {
         Scanner input = new Scanner(System.in);
         System.out.print("输入圆柱体的h:");
         double h = input.nextDouble();
-        System.out.print("输入圆柱体底圆的r：");
+        System.out.print("输入圆柱体底面的r：");
         double r = input.nextDouble();
-        //侧面积+两个底面积 == 2*PI*r+PI*r*r*2
-        System.out.printf("圆柱体的表面积为：%.2f%n", 2*pi*r+pi*r*r*2);
+        System.out.printf("圆柱体的表面积为：%.2f%n", 2*pi*r*h+pi*r*r*2);
     }
 }
