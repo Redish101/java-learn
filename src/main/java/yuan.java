cae76505657fb6554ec8afe527839a0b1990da2e
Redish101 <jiayunluo@outlook.com>
2022-04-08 09:51:43 +0800

    b

--- src/main/java/yuan.java
+++ src/main/java/yuan.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class yuan {
     public static void main(String[] args) {
-        double pi = 3.14;
-        System.out.print("请输入圆的半径：");
-        Scanner r = new Scanner(System.in);
-        double s = r.nextDouble() * pi;
-        System.out.println("这个圆的面积是:"+s);
+        final double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.print("输入圆柱体的h:");
+        double h = input.nextDouble();
+        System.out.print("输入圆柱体底圆的r：");
+        double r = input.nextDouble();
+        //侧面积+两个底面积 == 2*PI*r+PI*r*r*2
+        System.out.printf("圆柱体的表面积为：%.2f%n", 2*pi*r+pi*r*r*2);
     }
 }
